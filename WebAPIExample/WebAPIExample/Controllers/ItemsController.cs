10432e7ffa41d6a418bfc738106b29e58e4a2884
Eduardo Jaime <eduardojaime86@gmail.com>
2020-05-05 19:34:06 -0400

    Added webapi example project

---
+++ WebAPIExample/WebAPIExample/Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIExample.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        // Option 2 > Use decorators
+        [HttpGet]
+        public List<string> ObtainAllItems()
+        {
+            return new List<string>();
+        }
+
+        [HttpGet]
+        public string FilterItemsById(int id)
+        {
+            return "Lenovo T480s";
+        }
+
+        [HttpDelete]
+        public bool RemoveItemFromDb(int id)
+        {
+            return true;
+        }
+
+    }
+}
